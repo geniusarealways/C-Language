5986e27bc91b3ff032e2cefc485d3b4f4d9b4c82
geniusarealways <88645940+geniusarealways@users.noreply.github.com>
2024-09-21 13:43:03 +0530

    Add files via upload
    Question for concept building of how programming is differ with mathematics.

---
+++ question1.c
@@ -0,0 +1,12 @@
+//Q.) Predict the output in dry run and then run the original program
+#include<stdio.h>
+void main()
+{
+	int i=2,j=3,k,l;
+	float a,b;
+	k=i/j*j;
+	l=j/i*i;
+	a=i/j*j;
+	b=j/i*i;
+	printf("%d\n%d\n%f\n%f",k,l,a,b);
+ } 
