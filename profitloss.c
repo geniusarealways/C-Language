0c6dd37821a1164f3219442a287995f30abd754a
geniusarealways <88645940+geniusarealways@users.noreply.github.com>
2024-09-24 19:57:45 +0530

    Add files via upload
    Program to calculate the profit and loss using if else.

---
+++ profitloss.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+	int cp,sp;
+	printf("Enter Cost Price and Selling Price of Product:");
+	scanf("%d%d",&cp,&sp);
+	if(sp-cp>0){printf("Profit");
+	}
+	if(sp=cp){printf("Neither Loss nor Profit");
+	}
+		else{printf("Loss");
+	}
+}
